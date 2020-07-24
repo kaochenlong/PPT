218a0a8e29940292185f54749364b52da79f16b6
Eddie Kao <eddie@digik.com.tw>
2020-07-24 16:03:03 +0800

    add show page for Board

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+  def not_found
+    render file: '/public/404.html', status: 404
+  end
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,10 @@ class BoardsController < ApplicationController
     @boards = Board.all
   end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+
   def new
     @board = Board.new
   end
