d3f2381d22936737fde9c77a1639be326dece691
Eddie Kao <eddie@digik.com.tw>
2020-07-24 16:53:45 +0800

    delete Board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -35,6 +35,12 @@ class BoardsController < ApplicationController
     end
   end
 
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path, notice: "刪除成功"
+  end
+
   private
   # Strong Parameters
   def board_params
