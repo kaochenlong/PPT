dfc9eed9e8a142eaecd303b401ac9f95124bf424
Eddie Kao <eddie@digik.com.tw>
2020-07-30 10:24:18 +0800

    CRUD done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,11 @@
 class BoardsController < ApplicationController
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
+
   def index
     @boards = Board.all
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def new
@@ -22,12 +23,9 @@ class BoardsController < ApplicationController
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    @board = Board.find(params[:id])
-
     if @board.update(board_params)
       redirect_to boards_path, notice: "更新成功"
     else
@@ -36,12 +34,15 @@ class BoardsController < ApplicationController
   end
 
   def destroy
-    @board = Board.find(params[:id])
     @board.destroy
     redirect_to boards_path, notice: "刪除成功"
   end
 
   private
+  def find_board
+    @board = Board.find(params[:id])
+  end
+
   # Strong Parameters
   def board_params
     params.require(:board).permit(:title, :intro)
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,11 @@
 class Board < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
+
+  def destroy
+    update(deleted_at: Time.now)
+  end
+
+  # def self.all
+  #   where(deleted_at: nil)
+  # end
 end
