3c2c88123b041420db378a76011dbaae04e9c3dc
Eddie Kao <eddie@digik.com.tw>
2020-07-30 11:58:47 +0800

    add paranoia gem

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,11 +1,5 @@
 class Board < ApplicationRecord
+  acts_as_paranoid
+
   validates :title, presence: true, length: { minimum: 2 }
-
-  def destroy
-    update(deleted_at: Time.now)
-  end
-
-  # def self.all
-  #   where(deleted_at: nil)
-  # end
 end
