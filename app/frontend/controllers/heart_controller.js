bdde2e41e57efeb224da8259e54fc3aedd4c0f0f
Eddie Kao <eddie@digik.com.tw>
2020-08-10 16:43:54 +0800

    replace axios with rails/ujs

--- app/frontend/controllers/heart_controller.js
+++ app/frontend/controllers/heart_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "stimulus"
-import ax from "axios";
+// import Rails from "@rails/ujs";
 
 export default class extends Controller {
   static targets = [ "heart" ]
@@ -7,21 +7,21 @@ export default class extends Controller {
   favorite() {
     let board_id = this.data.get("board");
 
-    const token = document.querySelector("meta[name=csrf-token]").content;
-    ax.defaults.headers.common['X-CSRF-Token'] = token;
-
-    ax.post(`/boards/${board_id}/favorite.json`)
-      .then((result) => {
-        if (result.data["status"] == true) {
+    Rails.ajax({
+      url: `/boards/${board_id}/favorite.json`,
+      type: 'post',
+      success: (result) => {
+        if (result["status"] == true) {
           this.heartTarget.classList.remove("far");
           this.heartTarget.classList.add("fas");
         } else {
           this.heartTarget.classList.remove("fas");
           this.heartTarget.classList.add("far");
         }
-      })
-      .catch(function(err) {
+      },
+      error: (err) => {
         console.log(err);
-      })
+      }
+    })
   }
 }
